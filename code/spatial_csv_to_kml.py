1ce415a65e8ba1fa368ab17715bfacb2d59a4a5d
Jiehua Chen <j@qe-design.com>
2015-07-17 14:28:46 +0300

    add generating 1k by 1k grid kml files for drone flight planning.

--- code/spatial_csv_to_kml.py
+++ code/spatial_csv_to_kml.py
@@ -2,6 +2,9 @@
 
 import os, sys, glob, shutil, getopt, re
 import csv
+from simplekml import Kml
+import pyproj
+
 
 def main():				
 	csv_files = glob.glob("csv/*.csv")
@@ -39,10 +42,9 @@ def csv_to_kml(input_filename):
 		<SimpleField name="Prob" type="float"></SimpleField>
 	</Schema>
 	""")
-        
-        gids_unique = set()
-        gids = []
-        locs_1k = []        
+	gids_unique = set()
+	gids = []
+	locs_1k = []        
 	# main loop 
 	for line in reader:
 
@@ -50,7 +52,7 @@ def csv_to_kml(input_filename):
 		kml_file.write('  <name>L0=%s</name>\n' % (line['L0']))
 		kml_file.write('\t<ExtendedData><SchemaData schemaUrl=\"#sample\">\n')
 		kml_file.write(' <SimpleField name="ID_unit">%s</SimpleField>\n' % (line['ID_unit']))
-		kml_file.write('  <SimpleField name="Prob_3_stage">%s</SimpleField>\n' % (line['Prob_ 3 _stage']))
+		kml_file.write('  <SimpleField name="Prob_3_stage">%s</SimpleField>\n' % (line['Prob_.3._stage']))
 		kml_file.write('  <SimpleField name="Prob">%s</SimpleField>\n' % (line['Prob']))
 		kml_file.write('\t\t</SchemaData></ExtendedData>\n')
 		kml_file.write("     <Point><coordinates>%s,%s</coordinates></Point>\n" % (line['x'], line['y']))
@@ -60,22 +62,28 @@ def csv_to_kml(input_filename):
 	        gids.append(line['GID'])
                 locs_1k.append([line['x_1k'], line['y_1k']])
 
-        gids_unique = list(gids_unique)
-        locs_1k_unique = []
-        for gid in gids_unique:
-            locs_1k_unique.append([locs_1k[k] for k, x in enumerate(map(lambda x: x==gid, gids)) if x][0])
-
-        for i, loc in enumerate(locs_1k_unique):
-		kml_file.write('  <Placemark>\n')
-		kml_file.write('  <name>GID=%s</name>\n' % (gids_unique[i]))
-		# kml_file.write(' <styleUrl>#grid1k</styleUrl>\n')
-		kml_file.write("     <Point><coordinates>%s,%s</coordinates></Point>\n" % (loc[0], loc[1]))
-		kml_file.write('  </Placemark>\n')
-
 	# epilogue
 	kml_file.write('\t</Document>\n\t</kml>')
 	csv_file.close()
 	kml_file.close()
+	gids_unique = list(gids_unique)
+	locs_1k_unique = []
+	for gid in gids_unique:
+	    locs_1k_unique.append([locs_1k[k] for k, x in enumerate(map(lambda x: x==gid, gids)) if x][0])
+	
+	for i, loc in enumerate(locs_1k_unique):
+	    kml=Kml()
+	    proj_para =  "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"
+	    project = pyproj.Proj(proj_para)
+	    loc_laea = list(project(loc[0], loc[1]))
+	    center_pt = kml.newpoint(name=gids_unique[i], description="1k by 1k grid", coords=[(loc[0], loc[1])])
+	    lin = kml.newlinestring(name="1k grid", description="A pathway in Kirstenbosch", coords=[project(loc_laea[0]-500, loc_laea[1]+500, inverse=True), 
+	                                                                                      project(loc_laea[0]+500, loc_laea[1]+500, inverse=True), 
+	                                                                                      project(loc_laea[0]+500, loc_laea[1]-500, inverse=True),
+	                                                                                      project(loc_laea[0]-500, loc_laea[1]-500, inverse=True), 
+	                                                                                      project(loc_laea[0]-500, loc_laea[1]+500, inverse=True)])
+	    
+	    kml.save("csv/"+gids_unique[i]+".kml")
 
 if __name__ == '__main__':
 	main()
